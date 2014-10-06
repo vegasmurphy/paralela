4c2e8d010989d119905663ee50c1440e09c96ca5
Vegas Murphy <vegasmurphy@hotmail.com>
2014-10-06 16:29:18 -0300

    Vegas Murphy - pasar algoritmo como parametro

--- TP2.c
+++ TP2.c
@@ -13,55 +13,62 @@ Programar tambien usando mensajes no bloqueantes simultaneos entre todos los nod
 
 int main(int argc, char* argv[])
 {
-	int  numtasks, my_rank, rc, next, prev, buf_recv; 
+	int  numtasks, my_rank, rc, next, prev, buf_recv,alg; 
    	char hostname[MPI_MAX_PROCESSOR_NAME];
    	MPI_Status status;
       MPI_Request request;
 
-   	rc = MPI_Init(&argc,&argv);
-   	if (rc != MPI_SUCCESS) 
-   	{
-     	printf ("Error al iniciar el programa.\n");
-     	MPI_Abort(MPI_COMM_WORLD, rc);
+      //data initialization
+    if (argc>1){
+       alg=atoi(argv[1]);
+    } else {
+       alg=1;
     }
+   
+      rc = MPI_Init(&argc,&argv);
+      if (rc != MPI_SUCCESS) 
+      {
+      printf ("Error al iniciar el programa.\n");
+      MPI_Abort(MPI_COMM_WORLD, rc);
+    }
+
 
    MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-  
-  
-   // Determino el nodo siguiente y previo correspondiente a cada uno
-   if(my_rank == 0)
-   		prev = numtasks - 1;
+  if(my_rank == 0)
+         prev = numtasks - 1;
    else
-   		prev = my_rank - 1;
+         prev = my_rank - 1;
    if(my_rank == numtasks-1)
-   		next = 0;
+         next = 0;
    else
-   		next = my_rank + 1;
+         next = my_rank + 1;
 
-      /*
-   	// Envio y recepcion de mensaje
-   	if(!my_rank%2)
-   	{
-   		MPI_Send(&my_rank,1,MPI_INT,next,1, MPI_COMM_WORLD);
-   		MPI_Recv (&buf_recv,1,MPI_INT,prev,1,MPI_COMM_WORLD,&status);
-   	}
-   	else
-   	{
-   		MPI_Recv (&buf_recv,1,MPI_INT,prev,1,MPI_COMM_WORLD,&status);
-   		MPI_Send(&my_rank,1,MPI_INT,next,1, MPI_COMM_WORLD);
-   	}
+    switch (alg){
+    case 1:
 
-   	printf ("Soy = %d y recibi de = %d \n", my_rank, buf_recv);
-
-      MPI_Barrier (MPI_COMM_WORLD);
-
-      */
       MPI_Isend(&my_rank,1,MPI_INT,next,1,MPI_COMM_WORLD,&request);
       MPI_Recv (&buf_recv,1,MPI_INT,prev,1,MPI_COMM_WORLD,&status);
 
       printf ("Soy = %d y recibi de = %d \n", my_rank, buf_recv);
 
+    break;
+    case 2:
+        if(!my_rank%2)
+      {
+         MPI_Send(&my_rank,1,MPI_INT,next,1, MPI_COMM_WORLD);
+         MPI_Recv (&buf_recv,1,MPI_INT,prev,1,MPI_COMM_WORLD,&status);
+      }
+      else
+      {
+         MPI_Recv (&buf_recv,1,MPI_INT,prev,1,MPI_COMM_WORLD,&status);
+         MPI_Send(&my_rank,1,MPI_INT,next,1, MPI_COMM_WORLD);
+      }
 
+      printf ("Soy = %d y recibi de = %d \n", my_rank, buf_recv);
+
+      MPI_Barrier (MPI_COMM_WORLD);
+    break;
+    break;}
    MPI_Finalize();	
 }
